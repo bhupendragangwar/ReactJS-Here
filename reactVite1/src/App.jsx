51a632115f002036d76114952e3e618cfcf9745b
Bhupendra Kumar <149204876+bhupendra0307@users.noreply.github.com>
2024-12-08 12:29:32 +0000

    first commit

---
+++ reactVite1/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h2>React Project with Vite</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
