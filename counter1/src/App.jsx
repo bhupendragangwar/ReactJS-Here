afcaf197451f4201f2a3e31a08662992eabcfd5d
Bhupendra Kumar <149204876+bhupendra0307@users.noreply.github.com>
2024-12-08 17:02:56 +0000

    useState

--- counter1/src/App.jsx
+++ counter1/src/App.jsx
@@ -2,45 +2,47 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const defaultValue = 10
+  const defaultValue = 5
   const [count, setCount] = useState(defaultValue)
   let currentValue = count;
 
-  const addValue = () =>{
+  const addValue = () => {
     let maxValue = 20;
-    if(currentValue < maxValue ){
+    if (currentValue < maxValue) {
       setCount(count + 1)
     }
   }
-  const removeValue = () =>{
+  const removeValue = () => {
     let minValue = 0
-    if(currentValue > minValue){
+    if (currentValue > minValue) {
       setCount(count - 1)
     }
   }
 
-  const refresh = () =>{
+  const refresh = () => {
     setCount(defaultValue)
   }
 
   return (
     <>
-      <h1>React Learn | counter Project | Understanding hooks</h1>
-      <h1>Counter Value: {count}</h1>
-      
-      <button onClick={addValue}>
-        Add value: {count}
-      </button>
-      <br/>
-      <br/>
-      <button onClick={removeValue}>
-        Decrease Value: {count}
-      </button>
-      <br></br><br />
-      <button onClick={refresh}>
-        refresh
-      </button>
-
+      <div id='text'>
+        <h1>React Learn | counter Project</h1>
+        <p>Counter Value: {count}</p>
+      </div>
+      <div id='Project-working'>
+        <button onClick={addValue}>
+          Add value: {count}
+        </button>
+        <br />
+        <br />
+        <button onClick={removeValue}>
+          Decrease Value: {count}
+        </button>
+        <br></br><br />
+        <button onClick={refresh}>
+          refresh
+        </button>
+      </div>
     </>
   )
 }
