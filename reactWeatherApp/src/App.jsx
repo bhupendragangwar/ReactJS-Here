1ee8600284c98b76e30d0da76a27b2222b3f9625
Bhupendra Kumar <149204876+bhupendra0307@users.noreply.github.com>
2024-12-14 11:01:55 +0000

    react Weather app

---
+++ reactWeatherApp/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <div className='card'>
+        <h2>React Weather Application</h2>
+        <div id='input-Field'>
+          
+        </div>
+        <div>
+          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
